f33b582a52123ffcfab7e3aac4ef929bbc71392a
tuankhanhh <ttuankhanh4@gmail.com>
2025-11-14 19:19:52 +0700

    up lại project backend

--- BackEnd/WEB_SHOPTHETHAO_API/WEB_SHOPTHETHAO_API/Controllers/BrandController.cs
+++ BackEnd/WEB_SHOPTHETHAO_API/WEB_SHOPTHETHAO_API/Controllers/BrandController.cs
@@ -1,27 +1,27 @@
-﻿//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.EntityFrameworkCore;
-//using WEB_SHOPTHETHAO_API.Models;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WEB_SHOPTHETHAO_API.Models;
 
-//[Route("api/[controller]")]
-//[ApiController]
-//public class BrandController : ControllerBase
-//{
-//    private readonly ApplicationDbContext _context;
+[Route("api/[controller]")]
+[ApiController]
+public class BrandController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
 
-//    public BrandController(ApplicationDbContext context)
-//    {
-//        _context = context;
-//    }
+    public BrandController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
 
-//    [HttpGet]
-//    public async Task<IActionResult> GetAll() =>
-//        Ok(await _context.Brands.ToListAsync());
+    [HttpGet]
+    public async Task<IActionResult> GetAll() =>
+        Ok(await _context.Brands.ToListAsync());
 
-//    [HttpPost]
-//    public async Task<IActionResult> Create(Brand model)
-//    {
-//        _context.Brands.Add(model);
-//        await _context.SaveChangesAsync();
-//        return Ok(model);
-//    }
-//}
+    [HttpPost]
+    public async Task<IActionResult> Create(Brand model)
+    {
+        _context.Brands.Add(model);
+        await _context.SaveChangesAsync();
+        return Ok(model);
+    }
+}
--- BackEnd/WEB_SHOPTHETHAO_API/WEB_SHOPTHETHAO_API/Controllers/ProductVariantController.cs
+++ BackEnd/WEB_SHOPTHETHAO_API/WEB_SHOPTHETHAO_API/Controllers/ProductVariantController.cs
@@ -1,35 +1,35 @@
-﻿//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.EntityFrameworkCore;
-//using WEB_SHOPTHETHAO_API.Models;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WEB_SHOPTHETHAO_API.Models;
 
-//[Route("api/[controller]")]
-//[ApiController]
-//public class ProductVariantController : ControllerBase
-//{
-//    private readonly ApplicationDbContext _context;
+[Route("api/[controller]")]
+[ApiController]
+public class ProductVariantController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
 
-//    public ProductVariantController(ApplicationDbContext context)
-//    {
-//        _context = context;
-//    }
+    public ProductVariantController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
 
-//    [HttpGet("product/{productId}")]
-//    public async Task<IActionResult> GetByProduct(int productId)
-//    {
-//        var list = await _context.ProductVariants
-//            .Where(x => x.ProductId == productId)
-//            .Include(x => x.Size)
-//            .Include(x => x.Color)
-//            .ToListAsync();
+    [HttpGet("product/{productId}")]
+    public async Task<IActionResult> GetByProduct(int productId)
+    {
+        var list = await _context.ProductVariants
+            .Where(x => x.ProductId == productId)
+            .Include(x => x.Size)
+            .Include(x => x.Color)
+            .ToListAsync();
 
-//        return Ok(list);
-//    }
+        return Ok(list);
+    }
 
-//    [HttpPost]
-//    public async Task<IActionResult> Create(ProductVariant model)
-//    {
-//        _context.ProductVariants.Add(model);
-//        await _context.SaveChangesAsync();
-//        return Ok(model);
-//    }
-//}
+    [HttpPost]
+    public async Task<IActionResult> Create(ProductVariant model)
+    {
+        _context.ProductVariants.Add(model);
+        await _context.SaveChangesAsync();
+        return Ok(model);
+    }
+}
