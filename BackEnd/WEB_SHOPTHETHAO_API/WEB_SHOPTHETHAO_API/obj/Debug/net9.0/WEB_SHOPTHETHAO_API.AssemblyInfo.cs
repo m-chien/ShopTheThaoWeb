e0807c45684ba14435207ba15dcb6116a9b63427
tienhienn <huynhletienhien@gmail.com>
2025-12-21 00:43:21 +0700

    up ignore

--- BackEnd/WEB_SHOPTHETHAO_API/WEB_SHOPTHETHAO_API/obj/Debug/net9.0/WEB_SHOPTHETHAO_API.AssemblyInfo.cs
+++ BackEnd/WEB_SHOPTHETHAO_API/WEB_SHOPTHETHAO_API/obj/Debug/net9.0/WEB_SHOPTHETHAO_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEB_SHOPTHETHAO_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7213f93f4ca3fd999331b0b8e412f96aacd0e643")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63016f769ec402d3bf21bf6092430e30bc269b7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEB_SHOPTHETHAO_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEB_SHOPTHETHAO_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
