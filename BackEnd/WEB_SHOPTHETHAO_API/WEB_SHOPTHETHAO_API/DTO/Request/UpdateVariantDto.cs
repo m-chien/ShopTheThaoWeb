acab4dd6914807ac80f26f4cd94424eff1aca19b
Nguyen Bao Trung <baotrunngnguyen909@gmail.com>
2025-12-21 15:26:10 +0700

    Trung gửi file word

--- BackEnd/WEB_SHOPTHETHAO_API/WEB_SHOPTHETHAO_API/DTO/Request/UpdateVariantDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WEB_SHOPTHETHAO_API.DTO.Request
-{
-    public class UpdateVariantDto
-    {
-        public decimal Price { get; set; }
-        public int StockQuantity { get; set; }
-    }
-}
