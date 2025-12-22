a29fe80d372cb254ac09c6d3c81b900a714da4f9
minh chien <chientranminh355@gmail.com>
2025-12-23 01:00:13 +0700

    thêm folder BE

---
+++ BackEnd/WEB_SHOPTHETHAO_API/WEB_SHOPTHETHAO_API/DTO/Request/LoginRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WEB_SHOPTHETHAO_API.DTO.Request
+{
+    public class LoginRequest
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+    }
+}
---
+++ BackEnd/WEB_SHOPTHETHAO_API/WEB_SHOPTHETHAO_API/DTO/Request/RefreshTokenRequest.cs
@@ -0,0 +1,8 @@
+namespace WEB_SHOPTHETHAO_API.DTO.Request
+{
+    // DTO để refresh token
+    public class RefreshTokenRequest
+    {
+        public string RefreshToken { get; set; }
+    }
+}
